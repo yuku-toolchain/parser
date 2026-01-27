50de260958c1422b2db17f91740154e86fc2ec4a
arshad <arshadpyaseen@gmail.com>
2026-01-28 00:17:07 +0530

    separate misc

--- test/load.ts
+++ test/load.ts
@@ -1,4 +1,9 @@
 import Bun from 'bun'
+import { rm } from 'node:fs/promises'
+import path from 'path'
+
+const TEST_SUITE_REPO_URL = "https://github.com/arshad-yaseen/typescript-estree-parser-test-suite"
+const REMOVE_FILES = ["README.md", ".gitignore"]
 
 const shouldLoad = !(await Bun.file('test/suite').exists())
 
@@ -8,12 +13,19 @@ if (!shouldLoad) {
 
 const dest = Bun.argv[2] || '.'
 
-const TEST_SUITE_REPO_URL = "https://github.com/arshad-yaseen/typescript-estree-parser-test-suite"
-
 console.log("Downloading test suite...")
 
 Bun.spawnSync({
   cmd: ["git", "clone", "--quiet", "--no-progress", "--single-branch", "--depth", "1", TEST_SUITE_REPO_URL, dest]
 })
+
+
+for (const fileToRemove of REMOVE_FILES) {
+  try {
+    await rm(path.join(dest, fileToRemove), { force: true })
+  } catch (err) {
+    console.warn(`Failed to remove file ${fileToRemove}:`, err)
+  }
+}
 
 console.log()
